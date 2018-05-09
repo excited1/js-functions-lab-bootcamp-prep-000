12863779efbf4e2bcb228e6c4e1d4eb6cf4a0917
margaretta hemmings <mbcc1905@hotmail.com>
2018-05-09 22:16:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function happyHolidaysTo(Name) {
 function happyHolidayTo(holiday, name) {
 return `Happy ${holiday}, ${name}!`;
 }
-function holiday Countdown (holiday, days)
-return `It's ${days}until ${holiday}!``
+function holidayCountdown(holiday, days)
+return `It's ${days} until ${holiday}!`
